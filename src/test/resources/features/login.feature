47ea93e9e81608b11ddda88896ef35778aab6786
Nat <nataliatornea85@gmail.com>
2021-11-25 18:06:56 +0100

    Initial commit: Framework created

---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,4 @@
+@login
+Feature: login
+  Scenario:
+    Given  User logs in with valid credentials
---
+++ target/default-html-reports/report.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/login.feature");
+formatter.feature({
+  "name": "login",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User logs in with valid credentials",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.Login_StepDefinitions.user_logs_in_with_valid_credentials()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
