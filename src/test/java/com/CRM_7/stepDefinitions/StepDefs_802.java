a32d15cdda648501eea317c92dbe15593d083119
Endalina <endalina28@gmail.com>
2021-12-01 23:46:38 +0100

    StepDefs_802 is created.

---
+++ src/test/java/com/CRM_7/stepDefinitions/StepDefs_802.java
@@ -0,0 +1,18 @@
+package com.CRM_7.stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs_802 {
+    @When("The user clicks on the like, he\\/she is able to like other reviewer's comments.")
+    public void the_user_clicks_on_the_like_he_she_is_able_to_like_other_reviewer_s_comments() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user clicks on the reply, he\\/she is able to make a comment on the reviewers's comments.")
+    public void the_user_clicks_on_the_reply_he_she_is_able_to_make_a_comment_on_the_reviewers_s_comments() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/@ZULL-801_enda.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/@ZULL-802_enda.feature");
 formatter.feature({
   "name": "",
   "description": "",
   "keyword": "Feature"
 });
 formatter.scenario({
-  "name": "User should be able to make a comment, like, or unfollow on other employees\u0027 posts.",
+  "name": "User should be able to like or make comments on all other reviewers\u0027 comments",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@ZULL-801"
+      "name": "@ZULL-802"
     }
   ]
 });
@@ -21,12 +21,14 @@ formatter.step({
   "name": "The user is on the Portal page.",
   "keyword": "Given "
 });
-formatter.match({});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_is_on_the_Portal_page()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "The user clicks on the comment, he/she is able to make a comment on the employee\u0027s posts.",
+  "name": "The user clicks on the like, he/she is able to like other reviewer\u0027s comments.",
   "keyword": "When "
 });
 formatter.match({});
@@ -34,15 +36,7 @@ formatter.result({
   "status": "undefined"
 });
 formatter.step({
-  "name": "The user clicks on the like, he/she is able to like the posts.",
-  "keyword": "When "
-});
-formatter.match({});
-formatter.result({
-  "status": "undefined"
-});
-formatter.step({
-  "name": "The user clicks on the unfollow, he/she is able to unfollow the user\u0027s posts.",
+  "name": "The user clicks on the reply, he/she is able to make a comment on the reviewers\u0027s comments.",
   "keyword": "Then "
 });
 formatter.match({});
