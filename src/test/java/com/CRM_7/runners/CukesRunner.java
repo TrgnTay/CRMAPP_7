fae98601a10edc73304a56bee6c8abce96a13acd
Endalina <endalina28@gmail.com>
2021-12-03 11:04:48 +0100

    All work is done

--- src/test/java/com/CRM_7/runners/CukesRunner.java
+++ src/test/java/com/CRM_7/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/CRM_7/stepDefinitions",
         dryRun = false,
-        tags = "@ZULL-804"
+        tags = ""
 )
 public class CukesRunner {
 }
--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -1,4 +1,215 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/@ZULL-804_enda.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/@ZULL-801_enda.feature");
+formatter.feature({
+  "name": "User should be able to make a comment, like, or unfollow on other employees\u0027 posts.",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User logs in with valid credentials",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.Login_StepDefinitions.user_logs_in_with_valid_credentials()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to make a comment, like, or unfollow on other employees\u0027 posts.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@ZULL-801"
+    }
+  ]
+});
+formatter.step({
+  "name": "The user is on the Portal page.",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_is_on_the_Portal_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user clicks on the comment, the user is able to make a comment on the employees\u0027 posts.",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_clicks_on_the_comment_the_user_is_able_to_make_a_comment_on_the_employee_s_posts()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user clicks on the like, the user is able to like the posts.",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_clicks_on_the_like_the_user_is_able_to_like_the_posts()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user clicks on the unfollow, the user is able to unfollow the user\u0027s posts.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_clicks_on_the_unfollow_the_user_is_able_to_unfollow_the_user_s_posts()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/features/@ZULL-802_enda.feature");
+formatter.feature({
+  "name": "User should be able to like or make comments on all other reviewers\u0027 comments",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User logs in with valid credentials",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.Login_StepDefinitions.user_logs_in_with_valid_credentials()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to like or make comments on all other reviewers\u0027 comments",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@ZULL-802"
+    }
+  ]
+});
+formatter.step({
+  "name": "The user is on the Portal page.",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_is_on_the_Portal_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user clicks on the like, the user is able to like other reviewers\u0027 comments.",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_802.the_user_clicks_on_the_like_the_user_is_able_to_like_other_reviewer_s_comments()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user clicks on the reply, the user is able to make a comment on the reviewers\u0027s comments.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_802.the_user_clicks_on_the_reply_the_user_is_able_to_make_a_comment_on_the_reviewers_s_comments()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/features/@ZULL-803_enda.feature");
+formatter.feature({
+  "name": "User should be able to click on reviewers\u0027 names and visit their profiles.",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User logs in with valid credentials",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.Login_StepDefinitions.user_logs_in_with_valid_credentials()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "User should be able to click on reviewers\u0027 names and visit their profiles.",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@ZULL-803"
+    }
+  ]
+});
+formatter.step({
+  "name": "The user is on the Portal page.",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_801.the_user_is_on_the_Portal_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user is able to click on reviewers\u0027 names.",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_803.the_user_is_able_to_click_on_reviewers_names()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "The user is able to visit the reviewers\u0027 profiles.",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.StepDefs_803.the_user_is_able_to_visit_the_reviewers_profiles()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/features/@ZULL-804_enda.feature");
 formatter.feature({
   "name": "User should be able to add others\u0027 posts to favorite by clicking on the Star icon.",
   "description": "",
@@ -43,21 +254,58 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "The user clicks on the star, the user is able to click the star icon.",
+  "name": "The user clicks on the star.",
   "keyword": "When "
 });
 formatter.match({
-  "location": "com.CRM_7.stepDefinitions.StepDefs_804.the_user_clicks_on_the_star_the_user_is_able_to_click_the_star_icon()"
+  "location": "com.CRM_7.stepDefinitions.StepDefs_804.theUserClicksOnTheStar()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "The user sees romove from the favorites message, after clicking the star icon.",
+  "name": "The user is able to click the star icon.",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "com.CRM_7.stepDefinitions.StepDefs_804.the_user_sees_romove_from_the_favorites_message_after_clicking_the_star_icon()"
+  "location": "com.CRM_7.stepDefinitions.StepDefs_804.theUserIsAbleToClickTheStarIcon()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/features/login.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User logs in with valid credentials",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.CRM_7.stepDefinitions.Login_StepDefinitions.user_logs_in_with_valid_credentials()"
 });
 formatter.result({
   "status": "passed"
